e8a0e82a5bb7f08e9930d1a7b981705ce6f4d379
Jan Grape <jan.grape@crisp.se>
2021-05-07 18:11:11 +0200

    Optimized imports

--- src/main/java/se/accelerateit/signup6/dao/UserMapper.java
+++ src/main/java/se/accelerateit/signup6/dao/UserMapper.java
@@ -1,6 +1,8 @@
 package se.accelerateit.signup6.dao;
 
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import se.accelerateit.signup6.model.User;
 
 import java.util.List;
