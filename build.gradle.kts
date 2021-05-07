9e3f47a1ba6a468296c9cc56366ca31914f604b7
Jan Grape <jan.grape@crisp.se>
2021-05-07 18:12:53 +0200

    Gradle 7, using Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+  id("org.springframework.boot") version "2.4.1"
+  id("io.spring.dependency-management") version "1.0.10.RELEASE"
+  java
+}
+
+group = "se.accelerateit"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_16
+}
+
+configurations {
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-hateoas")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.flywaydb:flyway-core")
+  implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.4")
+
+  compileOnly("org.projectlombok:lombok:1.18.20")
+  annotationProcessor("org.projectlombok:lombok:1.18.20")
+
+  runtimeOnly("org.postgresql:postgresql")
+
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+  implementation(platform("org.testcontainers:testcontainers-bom:1.15.1")) //import bom
+  testImplementation("org.testcontainers:junit-jupiter:1.15.1")
+  testImplementation("org.testcontainers:postgresql:1.15.1")
+}
+
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
