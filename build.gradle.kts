010cf4098afc1335ac91f2fc45a4c8a3beb4769c
Jan Grape <jan.grape@crisp.se>
2021-10-14 17:08:34 +0200

    More explicit about version numbers

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,23 +23,23 @@ repositories {
 
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-hateoas")
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.flywaydb:flyway-core")
+  implementation("org.springframework.boot:spring-boot-starter-hateoas:2.5.5")
+  implementation("org.springframework.boot:spring-boot-starter-web:2.5.5")
+  implementation("org.flywaydb:flyway-core:7.14.1")
   implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0")
 
   compileOnly("org.projectlombok:lombok:1.18.22")
   annotationProcessor("org.projectlombok:lombok:1.18.20")
 
-  runtimeOnly("org.postgresql:postgresql")
+  implementation("org.postgresql:postgresql:42.2.24")
 
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  developmentOnly("org.springframework.boot:spring-boot-devtools:2.5.5")
 
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.5")
 
   implementation(platform("org.testcontainers:testcontainers-bom:1.16.0")) //import bill of materials (BOM)
-  testImplementation("org.testcontainers:junit-jupiter")
-  testImplementation("org.testcontainers:postgresql")
+  testImplementation("org.testcontainers:junit-jupiter:1.16.0")
+  testImplementation("org.testcontainers:postgresql:1.16.0")
 }
 
 
