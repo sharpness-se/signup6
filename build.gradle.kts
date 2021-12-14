7b0faf9b68aad1620ee5a25e668f690ff6fdc441
jgrape <jan.grape@accelerateit.se>
2021-12-14 16:36:24 +0100

    Let Spring Boot manage versions of as many dependencies as possible

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,23 +23,23 @@ repositories {
 
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-hateoas:2.6.1")
-  implementation("org.springframework.boot:spring-boot-starter-web:2.6.1")
-  implementation("org.flywaydb:flyway-core:8.2.1")
+  implementation("org.springframework.boot:spring-boot-starter-hateoas")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.flywaydb:flyway-core")
   implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0")
 
-  compileOnly("org.projectlombok:lombok:1.18.22")
-  annotationProcessor("org.projectlombok:lombok:1.18.22")
+  compileOnly("org.projectlombok:lombok")
+  annotationProcessor("org.projectlombok:lombok")
 
-  implementation("org.postgresql:postgresql:42.3.1")
+  implementation("org.postgresql:postgresql")
 
-  developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.1")
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-  testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.1")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
 
   implementation(platform("org.testcontainers:testcontainers-bom:1.16.2")) //import bill of materials (BOM)
-  testImplementation("org.testcontainers:junit-jupiter:1.16.2")
-  testImplementation("org.testcontainers:postgresql:1.16.2")
+  testImplementation("org.testcontainers:junit-jupiter")
+  testImplementation("org.testcontainers:postgresql")
 }
 
 
