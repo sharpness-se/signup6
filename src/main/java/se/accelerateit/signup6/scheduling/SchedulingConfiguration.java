a37b9429d2fc8eb5ae80f89eead223048890025a
JohanssonJacob <jacobjohanssn2001@gmail.com>
2022-10-21 15:37:33 +0200

    Added delay configuration in application.properties and alternate db conncetion in dev properties.

--- src/main/java/se/accelerateit/signup6/scheduling/SchedulingConfiguration.java
+++ src/main/java/se/accelerateit/signup6/scheduling/SchedulingConfiguration.java
@@ -1,6 +1,5 @@
 package se.accelerateit.signup6.scheduling;
 
-
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -9,30 +8,28 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.mail.MessagingException;
-import java.util.Date;
 
 @Configuration
 @EnableScheduling
-@ConditionalOnProperty(name= "scheduling.enabled", matchIfMissing = true)
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
 @Log4j2
 public class SchedulingConfiguration {
 
-    private final ScheduledEvents scheduledEvents;
+  private final ScheduledEvents scheduledEvents;
 
-    @Autowired
-    public SchedulingConfiguration(ScheduledEvents scheduledEvents){
-        this.scheduledEvents = scheduledEvents;
-    }
+  @Autowired
+  public SchedulingConfiguration(ScheduledEvents scheduledEvents) {
+    this.scheduledEvents = scheduledEvents;
+  }
 
-    // Janne: should be configurable in application.conf
-    @Scheduled(cron = "*/10 * * * * *") //(* = Sec | * = Min | * = Hour | * = Day | * = Month| * = DayOfWeeek)
-    public void scheduledTrigger() {
-        log.info("Time to send reminders!!");
-        try {
-            scheduledEvents.sendReminders();
-            log.debug("Reminders completed.");
-        } catch (MessagingException messagingException) {
-            log.error("WTF!!", messagingException);
-        }
+  @Scheduled(cron = "${reminderDelay}")
+  public void scheduledTrigger() {
+    log.info("Time to send reminders!!");
+    try {
+      scheduledEvents.sendReminders();
+      log.debug("Reminders completed.");
+    } catch (MessagingException messagingException) {
+      log.error("WTF!!", messagingException);
     }
+  }
 }
