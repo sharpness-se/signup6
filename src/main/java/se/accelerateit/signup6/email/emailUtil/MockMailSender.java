4d4f0ee757b2094eb02c2cd676483a34e4e6a5c4
Jan Grape <jan.grape@accelerateit.se>
2022-11-24 12:58:37 +0100

    Added possibility to inspect mock in a test case

--- src/main/java/se/accelerateit/signup6/email/emailUtil/MockMailSender.java
+++ src/main/java/se/accelerateit/signup6/email/emailUtil/MockMailSender.java
@@ -1,22 +1,38 @@
 package se.accelerateit.signup6.email.emailUtil;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 @Service
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class MockMailSender {
 
+  private List<String> recipients = new ArrayList<>();
+
+  public List<String> getAndClearRecipients() {
+    List<String> r = recipients;
+    recipients = new ArrayList<>();
+    return r;
+  }
   public void send(MimeMessage mimeMessage) throws MailException {
     try {
-      log.info("To: " + Arrays.toString(mimeMessage.getRecipients(Message.RecipientType.TO)));
+      String recipientsInThisMessage = Arrays.toString(mimeMessage.getRecipients(Message.RecipientType.TO));
+      recipients.add(recipientsInThisMessage);
+
+      log.info("To: " + recipientsInThisMessage);
       log.info("Message: " + mimeMessage.getContent().toString());
       log.info("------------------------------------");
     } catch (IOException | MessagingException e) {
