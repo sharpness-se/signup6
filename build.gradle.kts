6ad29859f2039bbabe4941cb27bbc1243a912588
Jan Grape <jan.grape@accelerateit.se>
2023-03-06 21:23:13 +0100

    Added springdoc-openapi-starter-webmvc-ui and spring-boot-starter-actuator to get Swagger going again.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,26 +26,25 @@ repositories {
 
 
 dependencies {
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
-
   // TODO: when we have Spring Security running, see https://springdoc.org/#spring-security-support
   //implementation("org.springdoc:springdoc-openapi-security:1.6.13")
 
-  implementation("org.springframework.boot:spring-boot-starter-hateoas")
   implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-hateoas")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
+  implementation("org.springframework.boot:spring-boot-starter-mail")
   implementation("org.springframework.boot:spring-boot-starter-freemarker")
+
+  implementation("org.postgresql:postgresql")
   implementation("org.flywaydb:flyway-core")
   implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.1")
-  implementation("org.springframework.boot:spring-boot-starter-mail")
 
   implementation("com.google.cloud:spring-cloud-gcp-starter-sql-postgresql:4.1.1")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
-
-  implementation("org.postgresql:postgresql")
-
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
 
