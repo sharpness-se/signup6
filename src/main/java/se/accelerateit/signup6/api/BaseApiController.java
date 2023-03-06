f22428b604f799122c859c86ae1623d3cde775dc
Jan Grape <jan.grape@accelerateit.se>
2023-03-06 21:20:33 +0100

    Allow CORS (Cross-Origin Resource Sharing)

--- src/main/java/se/accelerateit/signup6/api/BaseApiController.java
+++ src/main/java/se/accelerateit/signup6/api/BaseApiController.java
@@ -1,6 +1,8 @@
 package se.accelerateit.signup6.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class BaseApiController {}
