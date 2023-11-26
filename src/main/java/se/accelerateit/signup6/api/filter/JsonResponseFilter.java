20b981536fb05714f5c1f7a19554579a65a0bc21
Jan Grape <jan.grape@accelerateit.se>
2023-11-26 20:39:44 +0100

    Fixed bug in isLoggedIn() that didn't take into account that you can be "logged in" as an anonymous user (i.e. actually not logged in).

--- src/main/java/se/accelerateit/signup6/api/filter/JsonResponseFilter.java
+++ src/main/java/se/accelerateit/signup6/api/filter/JsonResponseFilter.java
@@ -6,12 +6,17 @@ import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractMappingJacksonResponseBodyAdvice;
 import se.accelerateit.signup6.model.Permission;
 import se.accelerateit.signup6.model.Visibility;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Check out <a href="https://www.baeldung.com/spring-security-role-filter-json">Filtering Jackson JSON Output Based on Spring Security Role</a>
@@ -19,9 +24,19 @@ import se.accelerateit.signup6.model.Visibility;
 @RestControllerAdvice
 class JsonResponseFilter extends AbstractMappingJacksonResponseBodyAdvice {
 
+  private static final List<String> availableAuthorities = Arrays.stream(Permission.values()).map(Enum::name).toList();
+
+
   private boolean isLoggedIn(Authentication authentication) {
-    return authentication != null;
+    if(authentication == null
+       || authentication instanceof AnonymousAuthenticationToken
+       || !authentication.isAuthenticated()) {
+      return false;
+    }
+    var userAuthorities = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+    return userAuthorities.stream().anyMatch(availableAuthorities::contains);
   }
+
   private boolean isAdministrator(Authentication authentication) {
     return authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals(Permission.Administrator.name()));
   }
